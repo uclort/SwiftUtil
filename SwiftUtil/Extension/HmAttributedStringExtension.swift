72cf7b5b2fe6adc425037eee21eebd0c4e422c77
Meng.Hou <uclort@gmail.com>
2021-05-14 15:13:03 +0800

    优化代码

--- SwiftUtil/Extension/HmAttributedStringExtension.swift
+++ SwiftUtil/Extension/HmAttributedStringExtension.swift
@@ -8,21 +8,19 @@
 import UIKit
 
 extension NSMutableAttributedString {
-    
     func addAttributeColor(_ color: UIColor, rangeString: String) {
         addAttribute(.foregroundColor, value: color, range: range(of: rangeString))
     }
-    
+
     func addAttributeFont(_ font: UIFont, rangeString: String) {
         addAttribute(.font, value: font, range: range(of: rangeString))
     }
-    
-    func addAttribute(_ attrs: [NSAttributedString.Key : Any], rangeString: String) {
+
+    func addAttribute(_ attrs: [NSAttributedString.Key: Any], rangeString: String) {
         addAttributes(attrs, range: range(of: rangeString))
     }
-    
+
     func range(of string: String = "") -> NSRange {
         self.string.range(of: string)
     }
-    
 }
